5bbed55eeb905624672404be048f34c33fbebbff
Christos Siannas <xrsia@otenet.gr>
2026-03-10 13:31:22 +0200

    cargo r -n Hello world

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,12 @@ fn main() {
     println!("{:?}",std::env::args());
 }
 /*
-Output:
+cargo r Hello world -n
 Args { inner: ["target/debug/echor", "Hello", "world", "-n"] }
+
+-------------
+cargo r -n Hello world
+Usage: run [OPTIONS] [ARGS]...
+
+For more information, try '--help'
 */
