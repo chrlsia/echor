b439caa24bd839ed346e980047f453b0d2284b7d
Christos Siannas <xrsia@otenet.gr>
2026-03-10 13:48:15 +0200

    first step ok

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use clap::Command;
+
 fn main() {
-    
+    let _matches= Command::new("echor")
+        .version("0.1.0")
+        .author("Chris Siannas <xrsia@otenet.gr>")
+        .about("Rust version of `echo`")
+        .get_matches();
+
 }
+/*
+cargo run -q -- -h
+Rust version of `echo`
+
+Usage: echor
+
+Options:
+  -h, --help     Print help
+  -V, --version  Print version
+*/
