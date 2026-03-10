1034c7414a34b228db994fcf41f05428c175facd
Christos Siannas <xrsia@otenet.gr>
2026-03-10 19:04:18 +0200

    create arg and Arg

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,33 @@
-use clap::Command;
+use clap::{Arg,ArgAction,Command};
 
 fn main() {
-    let _matches= Command::new("echor")
+    let matches= Command::new("echor")
         .version("0.1.0")
         .author("Chris Siannas <xrsia@otenet.gr>")
         .about("Rust version of `echo`")
+        .arg(
+            Arg::new("text")
+            .value_name("TEXT")
+            .help("Input text")
+            .required(true)
+            .num_args(1..),
+        )
+        .arg(
+            Arg::new("omit_newline")
+            .short('n')
+            .action(ArgAction::SetTrue)
+            .help("Do not print newline"),
+        )
         .get_matches();
+
+    let text: Vec<&String> = matches
+        .get_many::<String>("text")
+        .unwrap()
+        .collect();
+
+    let omit_newline = matches.get_flag("omit_newline");
+
+    println!("text: {:?}", text);
+    println!("omit_newline: {}", omit_newline);
 
 }
