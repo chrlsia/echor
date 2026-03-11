99dda3144734a2f174f715f5102867e153ad80e9
Christos Siannas <xrsia@otenet.gr>
2026-03-11 10:34:06 +0200

    dies_no_args pass

---
+++ tests/cli.rs
@@ -0,0 +1,17 @@
+use assert_cmd::Command;
+use predicates::prelude::*;
+
+#[test]
+fn dies_no_args(){
+    // mesa apo to current crate pare thn command echor
+    let mut cmd=Command::cargo_bin("echor").unwrap();
+    // exv thn command sthn cmd,cmd
+    // trejthn , assert
+    // bebaivsou oti apetyxe, failure
+    // afoy apetyxe,grace to apotelesma sto stderr, stderr
+    // kai ayto poy grafthke sto stderr periexei thn
+    // lejh Usage,predicate::str::contains("Usage")
+    cmd.assert()
+        .failure()
+        .stderr(predicate::str::contains("Usage"));
+}
