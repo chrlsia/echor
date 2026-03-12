21b1d4f2da2f429378464c019ae5bdec467ebf69
Christos Siannas <xrsia@otenet.gr>
2026-03-12 07:55:20 +0200

    run tests, ok (see output in comments)

--- tests/cli.rs
+++ tests/cli.rs
@@ -41,23 +41,31 @@ fn dies_no_args()->Result<()>{
 
 #[test]
 fn hello1()->Result<()>{
-    let outfile="tests/expected/hello1.txt";
-    let expected=fs::read_to_string(outfile)?;
-    dbg!(&expected);
-    let mut cmd=Command::cargo_bin("echor")?;
-    cmd.arg("Hello there").assert().success().stdout(expected);
-    Ok(())
+    run(&["Hello there"],"tests/expected/hello1.txt")
 
 }
 
 #[test]
 fn hello2()->Result<()>{
-    let outfile="tests/expected/hello2.txt";
-    let expected=fs::read_to_string(outfile)?;
-    dbg!(&expected);
-    let mut cmd=Command::cargo_bin("echor")?;
-    // here is .args 
-    cmd.args(vec!["Hello","there"]).assert().success().stdout(expected);
-    Ok(())
+    run(&["Hello","there"],"tests/expected/hello2.txt")
 
 }
+
+#[test]
+fn test_hello1_no_newline()->Result<()>{
+    run(&["Hello there","-n"],"tests/expected/hello1.n.txt")
+}
+
+#[test]
+fn test_hello2_no_newline()->Result<()>{
+    run(&["-n","Hello","there"],"tests/expected/hello2.n.txt")
+}
+
+/*
+cargo testdox
+✔ dies no args
+✔ hello1
+✔ hello2
+✔ test hello1 no newline
+✔ test hello2 no newline
+*/
